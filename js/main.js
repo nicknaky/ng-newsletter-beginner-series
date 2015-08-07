f965e20bc5a5f6c87bcf5c3f2c228f286f144e39
nicknaky <nicknaky@users.noreply.github.com>
2015-08-07 10:22:33 -0700

    Added custom directive, works fine in firefox but not chrome

--- js/main.js
+++ js/main.js
@@ -4,27 +4,51 @@ var apiKey = "MDIwMDkyNDQ5MDE0Mzg4NDAxMDg5MzI3Nw001";
 var	nprUrl = "http://api.npr.org/query?id=3&fields=title,byline,text,audio,image,pullQuote,relatedLink,all&dataType=story&output=JSON";      
 
 
+app.directive("nprLink", function() {
+  return {
+    restrict: 'EA',
+    require: ['^ngModel'],
+    replace: true,
+    scope: {
+      ngModel: '=',
+      play: '&'
+    },
+    templateUrl: 'views/nprListItem.html',
+    link: function(scope, ele, attr) {
+      scope.duration = scope.ngModel.audio[0].duration.$text;
+    }
+  }
+});
+
+
 app.controller("PlayerController", function($scope, $http) {
+  var audio = document.createElement("audio");
+  $scope.audio = audio;
 
   $http({
     method: "JSONP",
-    
     url: nprUrl + "&apiKey=" + apiKey + "&callback=JSON_CALLBACK"
-  }).then(function(file, status) {
-    var url = nprUrl + "&apiKey=" + apiKey + "&callback=JSON_CALLBACK";
+  }).then(function(response, status) {
     
-
-    $scope.programs = JSON.stringify(file.data.list.story);
-    console.log(file);
-    
-
-    //$scope.programs = JSON.stringify(data);
-    //console.log($scope.programs);
-  }, function(data, status) {
-
+    console.log(response);
+    $scope.programs = response.data.list.story;    
+  }, function(response, status) {
+    console.log('error', status, response);
   });
-});
 
+  $scope.play = function(program) {
+    if ($scope.playing) {
+      $scope.audio.pause();
+
+    }
+    var url = program.audio[0].format.mp4.$text;
+    audio.src = url;
+    audio.play();
+
+    $scope.playing = true;
+  } 
+
+});
 
 
 
