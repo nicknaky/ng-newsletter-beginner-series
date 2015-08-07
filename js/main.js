9f92f36c7db29f461497b48424660264061a9cca
nicknaky <nicknaky@users.noreply.github.com>
2015-08-07 16:41:25 -0700

    Fixed .then() call not displaying object correctly

--- js/main.js
+++ js/main.js
@@ -18,34 +18,31 @@ app.factory("githubService", ["$http", function($http) {
     return {
       events: function(username) { return doRequest(username, "events"); },
     };
-  }]);
-
-/*
-app.controller("ServiceController", ["$scope", "githubService", function($scope, githubService) {
-  $scope.$watch("username", function(newUserName) {
-    githubService.events(newUserName).then(function(data, status, headers) {
-      $scope.events = data;
-    });
-  });
 }]);
-*/
 
 
-app.controller('ServiceController', ['$scope', 'githubService',
-    function($scope, githubService) {
-    // Watch for changes on the username property.
-    // If there is a change, run the function
-    $scope.$watch('username', function(newUsername) {
-            // uses the $http service to call the GitHub API
-            // and returns the resulting promise
-      githubService.events(newUsername)
-        .success(function(data, status, headers) {
-                    // the success function wraps the response in data
-                    // so we need to call data.data to fetch the raw data
-          $scope.events = data.data;
-        })
-    });
+app.controller('ServiceController', ['$scope', "$timeout", 'githubService',
+    function($scope, $timeout, githubService) {
+      $scope.$watch('username', function(newUsername) {
+        githubService.events(newUsername)
+          .then(function(response) {
+            $scope.events = response.data.data;
+          });
+      });
+      var timeout;
+      $scope.$watch("username", function(newVal) {
+        if (newVal) {
+          if (timeout) $timeout.cancel(timeout);
+          timeout = $timeout(function() {
+            githubService.events(newVal)
+            .then(function(dat, status) {
+              $scope.events = response.data.data;
+            });
+          }, 350);
+        }
+      });
 }]);
+
 
 
 app.directive("nprLink", function() {
