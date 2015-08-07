3b00946ea061c353e4b9a02a25a4cede7ce17d1e
nicknaky <nicknaky@users.noreply.github.com>
2015-08-07 12:29:47 -0700

    Added GitHub service API example

--- js/main.js
+++ js/main.js
@@ -4,6 +4,50 @@ var apiKey = "MDIwMDkyNDQ5MDE0Mzg4NDAxMDg5MzI3Nw001";
 var	nprUrl = "http://api.npr.org/query?id=3&fields=title,byline,text,audio,image,pullQuote,relatedLink,all&dataType=story&output=JSON";      
 
 
+
+app.factory("githubService", ["$http", function($http) {
+    
+    var doRequest = function(username, path) {
+      return $http({
+        method: "JSONP",
+        url: "https://api.github.com/users/" + username + "/" + path + "?callback=JSON_CALLBACK"
+      });
+    }
+
+
+    return {
+      events: function(username) { return doRequest(username, "events"); },
+    };
+  }]);
+
+/*
+app.controller("ServiceController", ["$scope", "githubService", function($scope, githubService) {
+  $scope.$watch("username", function(newUserName) {
+    githubService.events(newUserName).then(function(data, status, headers) {
+      $scope.events = data;
+    });
+  });
+}]);
+*/
+
+
+app.controller('ServiceController', ['$scope', 'githubService',
+    function($scope, githubService) {
+    // Watch for changes on the username property.
+    // If there is a change, run the function
+    $scope.$watch('username', function(newUsername) {
+            // uses the $http service to call the GitHub API
+            // and returns the resulting promise
+      githubService.events(newUsername)
+        .success(function(data, status, headers) {
+                    // the success function wraps the response in data
+                    // so we need to call data.data to fetch the raw data
+          $scope.events = data.data;
+        })
+    });
+}]);
+
+
 app.directive("nprLink", function() {
   return {
     restrict: 'EA',
@@ -19,7 +63,6 @@ app.directive("nprLink", function() {
     }
   }
 });
-
 
 app.controller("PlayerController", function($scope, $http) {
   var audio = document.createElement("audio");
@@ -47,8 +90,8 @@ app.controller("PlayerController", function($scope, $http) {
 
     $scope.playing = true;
   } 
-
 });
+
 
 
 
